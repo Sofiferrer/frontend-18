6707928a98944bb0cdf6bd0c60a849e116abb9bf
sofia <sofia@blockinar.com>
2023-06-07 22:16:08 -0300

    Resolucion de ejercicios 1 y 2 de variables y datos

---
+++ clase-1/ejercicios/script.js
@@ -0,0 +1,21 @@
+// Saludo
+// Crear un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
+
+// let nombre = prompt('Ingrese su nombre');
+// let apellido = prompt('Ingrese su apellido');
+// const saludo = 'bienvenide a Ada';
+
+// alert(`Hola ${nombre} ${apellido}, ${saludo}`);
+
+
+
+// Heladería
+// Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado, y luego muestre un mensaje que diga, por ejemplo, "Aquí tiene su helado de chocolate, dulce de leche y menta granizada".
+
+let primerSabor = prompt('Ingrese el primer sabor de helado');
+let segundoSabor = prompt('Ingrese segundo sabor');
+let tercerSabor = prompt('Ingrese el tercer sabor')
+
+alert(`Aqui tiene su helado de ${primerSabor}, ${segundoSabor} y ${tercerSabor}`)
+
+
