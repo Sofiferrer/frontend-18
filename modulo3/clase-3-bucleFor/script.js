49700fc3c108f97e7841c09e2259f06240178a35
sofia <sofia@blockinar.com>
2023-08-09 21:43:36 -0300

    Agrego funcion sumarImpares

--- modulo3/clase-3-bucleFor/script.js
+++ modulo3/clase-3-bucleFor/script.js
@@ -107,3 +107,22 @@ const ponerMayusculas = (array) => {
 };
 
 //console.log(ponerMayusculas(nombres));
+
+//Crear una función sumarImparesHasta que tome como argumento un número numero y que devuelva la suma de todos los impares empezando desde 0 hasta dicho numero inclusive.
+
+sumarImparesHasta = (numero) => {
+  let impares = [];
+  for (let i = 1; i <= numero; i++) {
+    if (i % 2 !== 0) {
+      impares.push(i);
+    }
+  }
+  let suma = impares.reduce((acc, item) => {
+    return acc + item;
+  });
+  console.log(suma);
+  return suma;
+};
+sumarImparesHasta(5); // 9 (1 + 3 + 5 = 9)
+sumarImparesHasta(13); // 49
+sumarImparesHasta(47); // 576
