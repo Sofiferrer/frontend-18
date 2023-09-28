944b3dffdc35f1f69fbc690be8f9a413cfe77a4e
sofia <sofia@blockinar.com>
2023-09-27 21:53:06 -0300

    Clase 2 filtros y paginador

--- modulo4/clase-1-introAPI/js/api.js
+++ modulo4/clase-1-introAPI/js/api.js
@@ -1,24 +1,32 @@
-const $ = (selector) => document.querySelector(selector);
-const $$ = (selector) => document.querySelectorAll(selector);
 const baseURL = "https://rickandmortyapi.com/api";
-
-const showView = (view) => {
-  $$(".view").forEach((view) => view.classList.add("visually-hidden"));
-  $(`#${view}`).classList.remove("visually-hidden");
-};
+//https://rickandmortyapi.com/api/character?page=19
+//https://rickandmortyapi.com/api/character/?name=summer
 
 const getCharacters = () => {
   fetch(`${baseURL}/character`) //promesa
     .then((response) => response.json()) // respuesta parseada
-    .then((data) => showCharacters(data.results)); // data
+    .then((data) => showCharacters(data.results)) // data
+    .catch((error) => alert(error));
 };
 
 const getCharacterById = (id) => {
   fetch(`${baseURL}/character/${id}`) //promesa
     .then((response) => response.json()) // respuesta parseada
-    .then((data) =>
-      id ? showCharacterDetail(data) : showCharacters(data.results)
-    ); // data
+    .then((data) => showCharacterDetail(data)) // data
+    .catch((error) => alert(error));
+};
+
+const pagination = (page) => {
+  fetch(`${baseURL}/character?page=${page}`)
+    .then((response) => response.json())
+    .then((data) => showCharacters(data.results));
+};
+
+const getFilterCharacters = (params) => {
+  fetch(`${baseURL}/character?${params}`)
+    .then((response) => response.json())
+    .then((data) => showCharacters(data.results))
+    .catch((error) => window.alert(`HAY ERROOOOR ${error}`));
 };
 
 // const getLocation = (location) => {
@@ -26,46 +34,5 @@ const getCharacterById = (id) => {
 //     .then((response) => response.json()) // respuesta parseada
 //     .then((data) => console.log(data)); // data
 // };
-
-const showCharacters = (characters) => {
-  $("#charcaters-container").innerHTML = "";
-  for (let { image, name, id } of characters) {
-    $("#charcaters-container").innerHTML += `
-        <div class="card col-3">
-        <img src="${image}" class="card-img-top" alt="character-picture" />
-        <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-        </div>
-        <a href="#" class='btn btn-info' onclick=getCharacterById(${id})>Ver detalles</a>
-    </div>
-    `;
-  }
-};
-
-const showCharacterDetail = ({
-  name,
-  image,
-  status,
-  species,
-  gender,
-  origin,
-}) => {
-  showView("character-detail");
-  $("#character-detail").innerHTML = `
-  <div class="card col-6">
-  <img src="${image}" class="card-img-top" alt="character-picture" />
-  <div class="card-body">
-      <h5 class="card-title">${name}</h5>
-      <ul>
-        <li>Estado: ${status}</li>
-        <li>Especie: ${species}</li>
-        <li>Genero: ${gender}</li>
-        <li>Origen: ${origin.name}</li>
-      </ul>
-  </div>
- 
-</div>
-`;
-};
 
 window.onload = getCharacters();
--- modulo4/clase-1-introAPI/js/main.js
+++ modulo4/clase-1-introAPI/js/main.js
@@ -1,1 +1,88 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
+const showView = (view) => {
+  $$(".view").forEach((view) => view.classList.add("visually-hidden"));
+  $(`#${view}`).classList.remove("visually-hidden");
+};
+
+let page = 1;
+
+$("#home-btn").addEventListener("click", () => getCharacters());
+
+const showCharacters = (characters) => {
+  $("#charcaters-container").innerHTML = "";
+  if (characters) {
+    showView("charcaters-container");
+    for (let { image, name, id } of characters) {
+      $("#charcaters-container").innerHTML += `
+              <div class="card col-3">
+              <img src="${image}" class="card-img-top" alt="character-picture" />
+              <div class="card-body">
+                  <h5 class="card-title">${name}</h5>
+              </div>
+              <a href="#" class='btn btn-info' onclick=getCharacterById(${id})>Ver detalles</a>
+          </div>
+          `;
+    }
+  } else {
+    showView("no-results");
+  }
+};
+
+const showCharacterDetail = ({
+  name,
+  image,
+  status,
+  species,
+  gender,
+  origin,
+}) => {
+  showView("character-detail");
+  $("#character-detail").innerHTML = `
+    <div class="card col-6">
+    <img src="${image}" class="card-img-top" alt="character-picture" />
+    <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <ul>
+          <li>Estado: ${status}</li>
+          <li>Especie: ${species}</li>
+          <li>Genero: ${gender}</li>
+          <li>Origen: ${origin.name}</li>
+        </ul>
+    </div>
+   
+  </div>
+  `;
+};
+
+const goNext = () => {
+  if (page < 42) {
+    page = page + 1;
+    $("#page-number").innerHTML = `${page}`;
+    pagination(page);
+  }
+};
+
+const goPrev = () => {
+  if (page > 1) {
+    page = page - 1;
+    $("#page-number").innerHTML = `${page}`;
+    pagination(page);
+  }
+};
+
+$("#page-number").innerHTML = `${page}`;
+$("#prev-btn").addEventListener("click", () => goPrev());
+$("#next-btn").addEventListener("click", () => goNext());
+
+const filterCharacters = () => {
+  let params = {
+    name: $("#name-input").value,
+    gender: $("#gender-select").value,
+    status: $("#status-select").value,
+  };
+  getFilterCharacters(new URLSearchParams(params).toString());
+};
+
+$("#search-btn").addEventListener("click", () => filterCharacters());
